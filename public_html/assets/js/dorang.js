c0404efc2038c052cd1177841e70a0fbb3fe3bd5
dennis-hammerschlag <dennishammerschlag01@gmail.com>
2022-11-30 14:44:38 +0200

    Clean x2" "

--- public_html/assets/js/dorang.js
+++ public_html/assets/js/dorang.js
@@ -1,17 +1,3 @@
-/*!
-=========================================================
-* Dorang Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
  // toggle 
 $(document).ready(function(){
     
@@ -39,8 +25,6 @@ $(document).ready(function(){
         $('body').removeClass('light-theme');
     });
 });
-
-
 
 // smooth scroll
 $(document).ready(function(){
